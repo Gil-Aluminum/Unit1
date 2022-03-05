7156045005a6414e7bc8823935b3aac87abdcdb2
Gil Noyman <yiftahnoyman@Iftahs-MacBook-Pro.local>
2022-03-05 21:01:42 +0200

    project draft

--- js/script.js
+++ js/script.js
@@ -8,21 +8,118 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes array, 10 best The Office quotes which will show randomly.
 ***/
-
-
-
+const quotes = [
+  {
+    quote: "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
+    source: "Michael Scott",
+    citation: "The Office",
+    year: 2008,
+  } ,
+   {
+    quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+    source: "Michael Scott",
+    citation: "The Office",
+    year: 2005,
+  } ,
+   {
+    quote: "If I don’t have some cake soon, I might die.",
+    source: "Stanley Hudson",
+    citation: "The Office",
+    year: 2007,
+  } ,
+   {
+    quote: "The worst thing about prison was the dementors.",
+    source: 'Michael “Prison Mike” Scott',
+    citation: "The Office",
+    year: 2003,
+  } ,
+   {
+    quote: "No, Rose, they are not breathing. And they have no arms or legs … Where are they? You know what? If we come across somebody with no arms or legs, do we bother resuscitating them? I mean, what quality of life do we have there?",
+    source: "Michael Scott",
+    citation: "The Office",
+    year: 2005,
+  } ,
+   {
+    quote: "Identity theft is not a joke, Jim! Millions of families suffer every year.",
+    source: "Dwight Schrute",
+    citation: "The Office",
+    year: 2006,
+  } ,
+   {
+    quote: "Today, smoking is going to save lives.",
+    source: "Dwight Schrute",
+    citation: "The Office",
+    year: 2008,
+    tag: ",Best scene!"
+  } ,
+   {
+    quote: "I am running away from my responsibilities. And it feels good.",
+    source: "Michael Scott",
+    citation: "The Office",
+    year: 2007,
+  } ,
+   {
+    quote: "I just want to lie on the beach and eat hot dogs. That’s all I’ve ever wanted.",
+    source: "Kevin Malone",
+    citation: "The Office",
+    year: 2006,
+  } ,
+   {
+    quote: "If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.",
+    source: "Dwight Schrute",
+    citation: "The Office",
+    year: 2005,
+  }
+]
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function - this function returns a random quote through genarting a random number.
 ***/
 
+function getRandomQuote (){
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
 
-
+// *just checking!
+//console.log (getRandomQuote())
 /***
- * `printQuote` function
+ * `printQuote` function - this function print out a random quote to the html and adding citation year and tag if needed.
 ***/
 
+function printQuote (){
+  let randomQuote = getRandomQuote();
+  let html = `<p class="quote"> ${randomQuote.quote} </p>
+              <p class="source"> ${randomQuote.source}`
+                if (randomQuote.hasOwnProperty('citation')){
+                  html += `<span class="citation"> ${randomQuote.citation} </span>`}
+                
+                if (randomQuote.hasOwnProperty(`year`)) {
+                 html += `<span class="year"> ${randomQuote.year} </span>`
+                }
+
+                if (randomQuote.hasOwnProperty(`tag`)) {
+                 html += `<span class="tag"> ${randomQuote.tag} </span>`
+                }
+              
+  return html;        
+}
+
+
+// This function change the background color of the page randomly.
+let red = Math.floor(Math.random() * 256);
+let green = Math.floor(Math.random() * 256);
+let blue = Math.floor(Math.random() * 256);
+let randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+ document.body.style.backgroundColor = randomRGB;
+
+// This function refresh the page every 15 seconds (yes I did some digging online because we didn't learn it {https://stackoverflow.com/questions/32913226/auto-refresh-page-every-30-seconds#:~:text=If%20you%20really%20want%20to,(%20function()%20%7B%20window.})
+window.setTimeout( function() {
+  window.location.reload();
+}, 15000);
+
+   document.getElementById('quote-box').innerHTML = printQuote();    
 
 
 /***
